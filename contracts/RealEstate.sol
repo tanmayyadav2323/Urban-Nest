4a274f17475f0b2f16480fa4d70cbfac8b79d5ee
Tanmay Yadav <tanmayyadav2323@gmail.com>
2023-06-07 13:35:52 +0530

    Test Contract

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,23 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract RealEstate {
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+contract RealEstate is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Real Estate", "Real") {}
+
+    function mint(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -6,5 +6,52 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    let buyer, seller, inspector, lender;
+    let realEstate, escrow;
+
+    beforeEach(async()=>{
+        [buyer, seller, inspector, lender] = await ethers.getSigners();
+
+
+        //Deploy the contract
+        const RealEstate = await ethers.getContractFactory('RealEstate');
+        realEstate = await RealEstate.deploy();
+        console.log(realEstate);
+
+        //Mint the nft
+        let transaction = await realEstate.connect(seller).mint('https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/3.png');
+        await transaction.wait();
+
+        const Escrow = await ethers.getContractFactory('Escrow');
+        escrow = await Escrow.deploy(
+            realEstate.address,
+            seller.address,
+            inspector.address,
+            lender.address
+        );
+    })
+
+    describe('Deployment',()=>{
+        it('Returns NFT address',async()=>{
+            const result = await escrow.nftAddress();
+            expect(result).to.be.equal(realEstate.address);
+        })
+    
+        it("Returns seller", async() =>{
+           const result = await escrow.seller();
+            expect(result).to.be.equal(realEstate.seller);
+        })
+    
+        it("Returns inspector", async() =>{
+            const result = await escrow.inspector();
+            expect(result).to.be.equal(realEstate.inspector);
+        })
+    
+        it("Returns lender",async()=>{
+            const result = await escrow.lender();
+            expect(result).to.be.equal(realEstate.lender);
+        })    
+    });
+
 
 })
