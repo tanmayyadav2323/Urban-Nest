9c1b96f46270c2b84e282a87c770493681c54f3e
Tanmay Yadav <tanmayyadav2323@gmail.com>
2023-06-08 12:10:25 +0530

    Component: Nav Bar, Feat: Wallet Connected

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,10 +4,60 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+
 const hre = require("hardhat");
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
 
 async function main() {
+  let buyer, seller, inspector, lender;
+  [buyer, seller, inspector, lender] = await ethers.getSigners();
 
+  //Deploy realestate
+  const RealEstate = await ethers.getContractFactory('RealEstate');
+  const realEstate = await RealEstate.deploy();
+  await realEstate.deployed();
+
+  console.log(`Deployed Real Estate Contract at :${realEstate.address}`);
+  console.log(`Minting 3 properties`);
+
+  for (let i = 0; i < 3; i++) {
+    const transaction = await realEstate.connect(seller).mint(`https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/${i + 1}.json`);
+    await transaction.wait();
+  }
+
+
+  //Deploy Escrow
+  const Escrow = await ethers.getContractFactory('Escrow');
+  const escrow = await Escrow.deploy(
+    realEstate.address,
+    seller.address,
+    inspector.address,
+    lender.address
+  );
+  await escrow.deployed();
+
+  for (let i = 0; i < 3; i++) {
+    // Approve properties...
+    let transaction = await realEstate.connect(seller).approve(escrow.address, i + 1)
+    await transaction.wait()
+  }
+
+  // Listing properties...
+  let transaction = await escrow.connect(seller).list(1, tokens(20), buyer.address, tokens(10))
+  await transaction.wait()
+
+  transaction = await escrow.connect(seller).list(2, tokens(15), buyer.address, tokens(5))
+  await transaction.wait()
+
+  transaction = await escrow.connect(seller).list(3, tokens(10), buyer.address, tokens(5))
+  await transaction.wait()
+
+
+  console.log('Finished.');
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- src/App.js
+++ src/App.js
@@ -14,16 +14,30 @@ import Escrow from './abis/Escrow.json'
 import config from './config.json';
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+
+  const loadBlockchainData = async () => {
+    const provider = await new ethers.providers.Web3Provider(window.ethereum);
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account);
+    })
+  }
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, [])
 
   return (
     <div>
-
       <div className='cards__section'>
-
-        <h3>Welcome to Millow</h3>
-
+        <Navigation account={account} setAccount={setAccount} />
+        <Search />
+        <h3>Welcome to Millow!</h3>
       </div>
-
     </div>
   );
 }
